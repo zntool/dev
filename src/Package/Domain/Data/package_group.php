271c3218d14829b4c59c20c581072e593f7958c2
Vitaliy <theyamshikov@yandex.ru>
2020-08-27 00:58:31 +0600

    upd

--- src/Package/Domain/Data/package_group.php
+++ src/Package/Domain/Data/package_group.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     [
         'name' => 'znbundle',
         'provider_name' => 'github',
@@ -17,12 +18,42 @@ return [
         'authors' => [],
     ],
     [
-        'name' => 'zntool',
+        'name' => 'zndev',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'zndoc',
         'provider_name' => 'github',
         'authors' => [],
     ],
     [
         'name' => 'znlib',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'znlab',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'znsandbox',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'zntpl',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'znexample',
+        'provider_name' => 'github',
+        'authors' => [],
+    ],
+    [
+        'name' => 'zntool',
         'provider_name' => 'github',
         'authors' => [],
     ],
@@ -54,36 +85,6 @@ return [
     ],
     [
         'name' => 'php7tool',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],
-    [
-        'name' => 'zndoc',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],
-    [
-        'name' => 'zndev',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],
-    [
-        'name' => 'znlab',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],
-    [
-        'name' => 'znsandbox',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],
-    /*[
-        'name' => 'zntool',
-        'provider_name' => 'github',
-        'authors' => [],
-    ],*/
-    [
-        'name' => 'zntpl',
         'provider_name' => 'github',
         'authors' => [],
     ],
