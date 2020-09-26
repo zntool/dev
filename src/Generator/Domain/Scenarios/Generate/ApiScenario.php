aac667539f743f4d28dbdae52a4ec44ff2fd8a1b
Vitaliy <theyamshikov@yandex.ru>
2020-09-26 23:32:31 +0600

    upd

--- src/Generator/Domain/Scenarios/Generate/ApiScenario.php
+++ src/Generator/Domain/Scenarios/Generate/ApiScenario.php
@@ -43,7 +43,7 @@ class ApiScenario extends BaseScenario
         }
 
         if ($this->buildDto->isCrudController) {
-            $fileGenerator->setUse('ZnLib\Rest\Base\BaseCrudApiController');
+            $fileGenerator->setUse('ZnLib\Rest\Symfony4\Base\BaseCrudApiController');
             $classGenerator->setExtendedClass('BaseCrudApiController');
         } else {
             $fileGenerator->setUse('Symfony\Bundle\FrameworkBundle\Controller\AbstractController');
