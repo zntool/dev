d4f3ad1a5f4dc53e47b3fd35ce65a6387a4c30ca
Vitaliy <theyamshikov@yandex.ru>
2021-01-08 22:17:09 +0600

    upd

--- src/Dumper/Domain/Facades/Bot.php
+++ src/Dumper/Domain/Facades/Bot.php
@@ -2,7 +2,8 @@
 
 namespace ZnTool\Dev\Dumper\Domain\Facades;
 
-use Illuminate\Container\Container;
+use Illuminate\Container\EntryNotFoundException;
+use ZnLib\Web\Symfony4\MicroApp\ContainerHelper;
 use ZnTool\Dev\Dumper\Domain\Repositories\Telegram\DumperRepository;
 
 class Bot
@@ -10,9 +11,18 @@ class Bot
 
     public static function send($message, $chatId = null)
     {
-        $container = Container::getInstance();
-        /** @var DumperRepository $repo */
-        $repo = $container->get(DumperRepository::class);
-        $repo->send($message, $chatId);
+        $dumperRepository = self::getRepository();
+        $dumperRepository->send($message, $chatId);
+    }
+
+    private static function getRepository(): DumperRepository
+    {
+        $container = ContainerHelper::getContainer();
+        try {
+            $dumperRepository = $container->get(DumperRepository::class);
+        } catch (EntryNotFoundException $e) {
+            $dumperRepository = new DumperRepository($_ENV['DUMPER_BOT_TOKEN'], $_ENV['DUMPER_BOT_ADMIN_ID']);
+        }
+        return $dumperRepository;
     }
 }
