d980d20778b4f2dad7dd920ff535e8633b0238a8
Vitaliy <theyamshikov@yandex.ru>
2021-01-15 15:16:38 +0600

    upd

--- src/Dumper/Domain/Facades/Bot.php
+++ src/Dumper/Domain/Facades/Bot.php
@@ -2,38 +2,8 @@
 
 namespace ZnTool\Dev\Dumper\Domain\Facades;
 
-use Illuminate\Container\EntryNotFoundException;
-use ZnCore\Base\Libs\App\Helpers\ContainerHelper;
-use ZnTool\Dev\Dumper\Domain\Repositories\Telegram\DumperRepository;
-
-class Bot
+/** @deprecated */
+class Bot extends \ZnLib\Telegram\Domain\Facades\Bot
 {
 
-    public static function dump($message, $chatId = null, bool $isEncode = true)
-    {
-        $dumperRepository = self::getRepository();
-        $dumperRepository->send($message, $chatId, $isEncode);
-    }
-
-    public static function send($message, $chatId = null, bool $isEncode = true)
-    {
-        self::dump($message, $chatId, $isEncode);
-    }
-
-    public static function sendAsString($message, $chatId = null)
-    {
-        $dumperRepository = self::getRepository();
-        $dumperRepository->send($message, $chatId, false);
-    }
-
-    private static function getRepository(): DumperRepository
-    {
-        $container = ContainerHelper::getContainer();
-        try {
-            $dumperRepository = $container->get(DumperRepository::class);
-        } catch (EntryNotFoundException $e) {
-            $dumperRepository = new DumperRepository($_ENV['DUMPER_BOT_TOKEN'], $_ENV['DUMPER_BOT_ADMIN_ID']);
-        }
-        return $dumperRepository;
-    }
 }
--- src/Dumper/Domain/config/container.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use ZnTool\Dev\Dumper\Domain\Repositories\Telegram\DumperRepository;
-
-return [
-    DumperRepository::class => function () {
-        return new DumperRepository($_ENV['DUMPER_BOT_TOKEN'], $_ENV['DUMPER_BOT_ADMIN_ID']);
-    },
-];
