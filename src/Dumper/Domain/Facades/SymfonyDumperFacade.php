13b8f69bd6e1975aec5f86fc8bd3b442e3e4f278
Vitaliy <theyamshikov@yandex.ru>
2021-02-16 17:51:28 +0600

    upd

---
+++ src/Dumper/Domain/Facades/SymfonyDumperFacade.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ZnTool\Dev\Dumper\Domain\Facades;
+
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\AbstractDumper;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\CliContextProvider;
+use Symfony\Component\VarDumper\Dumper\ContextProvider\SourceContextProvider;
+use Symfony\Component\VarDumper\Dumper\DataDumperInterface;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Dumper\ServerDumper;
+use Symfony\Component\VarDumper\VarDumper;
+use ZnCore\Base\Helpers\EnvHelper;
+
+class SymfonyDumperFacade
+{
+
+    const URL = 'tcp://127.0.0.1:9912';
+
+    public static function dumpInConsole()
+    {
+        VarDumper::setHandler([self::class, 'handler']);
+    }
+
+    public static function handler($var) {
+        $dumper = self::createServerDumper();
+        $cloner = new VarCloner();
+        $dumper->dump($cloner->cloneVar($var));
+    }
+
+    private static function createServerDumper(): DataDumperInterface
+    {
+        $fallbackDumper = self::getDumper();
+        $contextProviders = [
+            'cli' => new CliContextProvider(),
+            'source' => new SourceContextProvider(),
+        ];
+        return new ServerDumper(self::URL, $fallbackDumper, $contextProviders);
+    }
+
+    private static function getDumper(): AbstractDumper
+    {
+        return EnvHelper::isConsole() ? new CliDumper() : new HtmlDumper();
+    }
+
+    /*private static function isConsole(): bool
+    {
+        return in_array(PHP_SAPI, ['cli', 'phpdbg']);
+    }*/
+}
